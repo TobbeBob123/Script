e3dd8b5724b226bbef7a6ea539ce40661610d138
Tobias <tobias.finsbol@gmail.com>
2022-03-23 17:32:36 +0100

    Script

---
+++ LaptopBare.sh
@@ -0,0 +1,3 @@
+/home/tobias/.screenlayout/LaptopBare.sh
+feh --bg-scale Bakgrunner/wallpaper-3.jpg
+betterlockscreen -u Bakgrunner/wallpaper-5.jpg
---
+++ master-stack.sh
@@ -0,0 +1,1 @@
+/home/tobias/.config/i3/i3-master-stack/i3_master.sh
