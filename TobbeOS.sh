c95c884a36440f12b9c1779567e90fe55a157dbc
Tobias <tobias.finsbol@gmail.com>
2022-05-04 19:28:00 +0200

    TobbeOS

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -1,12 +1,12 @@
 sudo pacman -Sy && sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib yay xmobar kakoune && \
-yay -S htop kitty  dmenu trayer yad git zip feh scrot dunst pavucontrol nm-connection-editor vim libreoffice lux pulseaudio picom pcmanfm && \
+yay -S htop kitty network-manager-applet lxsession dmenu trayer yad git zip feh scrot dunst pavucontrol nm-connection-editor vim libreoffice pulseaudio picom pcmanfm && \
                  rm -rf ~/.xmonad && \
                  rm -rf ~/.config/kitty && \
                  rm -rf ~/.config/xmobar && \
                  git clone https://github.com/TobbeBob123/.xmonad.git ~/.xmonad && \
                  git clone https://github.com/TobbeBob123/xmobar.git ~/.config/xmobar && \
                  git clone https://github.com/TobbeBob123/kitty.git ~/.config/kitty && \
-                 git clone https://github.com/TobbeBob123/Bakgrunner.git ~ && \
+                 git clone https://github.com/TobbeBob123/Bakgrunner.git ~/Bakgrunner && \
                  feh --bg-scale ~/Bakgrunner/Arch_Dracula.png && \
                  xmonad --recompile && \
                  echo "Gratulerer! Du har nå installert TobbeOS"
