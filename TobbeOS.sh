6c6f67ef33a0e1ee2df9dd30dd5cedacf3ca381f
Tobias <tobias.finsbol@gmail.com>
2022-05-04 10:42:19 +0200

    TobbeOS

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -1,14 +1,12 @@
 sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib yay && \
-yay -S htop kitty dmenu trayer git feh scrot nerd-font-complete dunst pavucontrol nm-connetion-editor libreoffice lux pulseaudio picom pcmanfm && \
+yay -S htop kitty dmenu trayer git feh scrot nerd-font-complete dunst pavucontrol m-connection-editor libreoffice lux pulseaudio picom pcmanfm && \
                  rm -rf ~/.xmonad && \
                  rm -rf ~/.config/kitty && \
                  rm -rf ~/.config/xmobar && \
-                 git clone https://github.com/TobbeBob123/.xmonad.git && \
-                 git clone https://github.com/TobbeBob123/xmobar.git && \
-                 git clone https://github.com/TobbeBob123/kitty.git && \
-                 git clone https://github.com/TobbeBob123/Bakgrunner.git && \
-                 mv xmobar ~/.config && \
-                 mv kitty ~/.config && \
+                 git clone https://github.com/TobbeBob123/.xmonad.git ~/.xmonad && \
+                 git clone https://github.com/TobbeBob123/xmobar.git ~/.config/ && \
+                 git clone https://github.com/TobbeBob123/kitty.git ~/.config && \
+                 git clone https://github.com/TobbeBob123/Bakgrunner.git ~ && \
                  feh --bg-scale ~/Bakgrunner/Arch_Dracula.png && \
                  xmonad --recompile && \
                  echo "Gratulerer! Du har nå installert TobbeOS"
