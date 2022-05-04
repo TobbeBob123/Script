ac7f898b61c515026744a5eb048a64234d7e1e85
Tobias <tobias.finsbol@gmail.com>
2022-05-04 10:08:54 +0200

    TobbeOS

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -1,16 +1,16 @@
-sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib yay | \
-yay -S htop kitty dmenu trayer git feh scrot nerd-font-complete dunst pavucontrol nm-connetion-editor libreoffice lux pulseaudio picom pcmanfm | \
-                 rm -rf ~/.xmonad | \
-                 rm -rf ~/.config/kitty | \
-                 rm -rf ~/.config/xmobar | \
-                 git clone https://github.com/TobbeBob123/.xmonad.git | \
-                 git clone https://github.com/TobbeBob123/xmobar.git | \
-                 git clone https://github.com/TobbeBob123/kitty.git | \
-                 git clone https://github.com/TobbeBob123/Bakgrunner.git | \
-                 mv xmobar ~/.config | \
-                 mv kitty ~/.config | \
-                 feh --bg-scale ~/Bakgrunner/Arch_Dracula.png | \
-                 xmonad --recompile | \
+sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib yay && \
+yay -S htop kitty dmenu trayer git feh scrot nerd-font-complete dunst pavucontrol nm-connetion-editor libreoffice lux pulseaudio picom pcmanfm && \
+                 rm -rf ~/.xmonad && \
+                 rm -rf ~/.config/kitty && \
+                 rm -rf ~/.config/xmobar && \
+                 git clone https://github.com/TobbeBob123/.xmonad.git && \
+                 git clone https://github.com/TobbeBob123/xmobar.git && \
+                 git clone https://github.com/TobbeBob123/kitty.git && \
+                 git clone https://github.com/TobbeBob123/Bakgrunner.git && \
+                 mv xmobar ~/.config && \
+                 mv kitty ~/.config && \
+                 feh --bg-scale ~/Bakgrunner/Arch_Dracula.png && \
+                 xmonad --recompile && \
                  echo "Gratulerer! Du har nå installert TobbeOS"
                  
                                   
