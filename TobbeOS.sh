20aa48c939b6564d0b71c822e51d82f20240dbb0
Tobias <tobias.finsbol@gmail.com>
2022-05-04 20:28:30 +0200

    TobbeOS. Dracula Theme

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -1,5 +1,5 @@
 sudo pacman -Sy && sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib yay xmobar kakoune && \
-yay -S htop kitty network-manager-applet lxsession dmenu trayer yad git zip feh scrot dunst pavucontrol nm-connection-editor vim libreoffice pulseaudio picom pcmanfm && \
+yay -S htop kitty network-manager-applet lxsession dmenu trayer yad dracula git zip feh scrot dunst pavucontrol nm-connection-editor vim libreoffice pulseaudio picom pcmanfm && \
                  rm -rf ~/.xmonad && \
                  rm -rf ~/.config/kitty && \
                  rm -rf ~/.config/xmobar && \
@@ -7,6 +7,9 @@ yay -S htop kitty network-manager-applet lxsession dmenu trayer yad git zip feh 
                  git clone https://github.com/TobbeBob123/xmobar.git ~/.config/xmobar && \
                  git clone https://github.com/TobbeBob123/kitty.git ~/.config/kitty && \
                  git clone https://github.com/TobbeBob123/Bakgrunner.git ~/Bakgrunner && \
+                 git clone https://github.com/TobbeBob123/Dracula_tema.git ~/.config/gtk-3.0 && \
+                 git clone https://github.com/TobbeBob123/dunst.git ~/.config/dunst && \
+                 mv ~/.config/gtk-3.0/Dracula /usr/share/themes/Dracula && \
                  feh --bg-scale ~/Bakgrunner/Arch_Dracula.png && \
                  xmonad --recompile && \
                  echo "Gratulerer! Du har nå installert TobbeOS"
