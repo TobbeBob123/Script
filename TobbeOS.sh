8576f61cc3555c4f461ee0d0ea12d1b51f7f6466
Tobias <tobias.finsbol@gmail.com>
2022-05-04 21:01:44 +0200

    TobbeOS. Dracula Theme

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -11,7 +11,7 @@ yay -S htop kitty network-manager-applet lxsession dmenu trayer yad git zip feh 
                  git clone https://github.com/TobbeBob123/Bakgrunner.git ~/Bakgrunner && \
                  git clone https://github.com/TobbeBob123/Dracula_tema.git ~/.config/gtk-3.0 && \
                  git clone https://github.com/TobbeBob123/dunst.git ~/.config/dunst && \
-                 mv ~/.config/gtk-3.0/Dracula /usr/share/themes/Dracula && \
+                 sudo mv ~/.config/gtk-3.0/Dracula /usr/share/themes/Dracula && \
                  feh --bg-scale ~/Bakgrunner/Arch_Dracula.png && \
                  xmonad --recompile && \
                  echo "Gratulerer! Du har nå installert TobbeOS"
