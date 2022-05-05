74359a8e3fa6774a119efdfad9f941a555326002
Tobias <tobias.finsbol@gmail.com>
2022-05-05 07:12:43 +0200

    Yay og rettet på feil

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -1,6 +1,6 @@
 sudo pacman -Sy && sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib xmobar kakoune xorg xf86-video-intel && \
 sudo rm -rf yay-git ~/yay-git
-git clone https://aur.archlinux.org/yay-git.git ~/yay-git && cd ~/yay-git && makepkg -si && \
+git clone https://aur.archlinux.org/yay-git.git ~/yay-git && cd ~/yay-git && makepkg -si && cd && rm -rf yay-git && \
 yay -S htop kitty network-manager-applet lxsession dmenu trayer yad git zip feh scrot dunst pavucontrol nm-connection-editor vim libreoffice pulseaudio picom pcmanfm && \
                  rm -rf ~/.xmonad && \
                  rm -rf ~/.config/kitty && \
@@ -8,6 +8,8 @@ yay -S htop kitty network-manager-applet lxsession dmenu trayer yad git zip feh 
                  rm -rf ~/.config/dunst && \
                  sudo rm -rf ~/.config/gtk-3.0 && \
                  rm -rf ~/Bakgrunner && \
+                 sudo rm -rf /usr/share/themes
+                 sudo mkdir /usr/share/themes
                  git clone https://github.com/TobbeBob123/.xmonad.git ~/.xmonad && \
                  git clone https://github.com/TobbeBob123/xmobar.git ~/.config/xmobar && \
                  git clone https://github.com/TobbeBob123/kitty.git ~/.config/kitty && \
