0634f85e0584725a1fbc012c684aabd29b06520c
Tobias <tobias.finsbol@gmail.com>
2022-05-05 20:12:47 +0200

    fish og diverse programmer

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -5,10 +5,13 @@ if [[ $svar == "n" ]] || [[ $svar == "N" ]]
 then
     exit
 else
-    sudo pacman -Sy && sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib xmobar kakoune xorg xorg-xinit xf86-video-intel && \
+    sudo pacman -Sy && sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib xmobar kakoune xorg xorg-xinit fish starship xf86-video-intel && \
     sudo rm -rf yay-git ~/yay-git && \
     git clone https://aur.archlinux.org/yay-git.git ~/yay-git && cd ~/yay-git && makepkg -si && cd && rm -rf yay-git && \
-    yay -S htop kitty network-manager-applet lxsession dmenu lux-git trayer yad git lightdm lightdm-gtk-greeter zip feh scrot dunst pavucontrol nm-connection-editor vim libreoffice pulseaudio picom pcmanfm && \
+    yay -S htop kitty network-manager-applet xautolock lxsession dmenu lux-git trayer yad git lightdm lightdm-gtk-greeter zip feh scrot dunst pavucontrol nm-connection-editor vim libreoffice librewolf-bin signal-desktop teams-for-linux thunderbird pulseaudio picom pcmanfm && \
+                 rm -rf ~/.config/fish && \
+                 rm -rf ~/starship && \
+                 rm -f ~/.config/starship.toml && \
                  rm -rf ~/.xmonad && \
                  rm -rf ~/.config/kitty && \
                  rm -rf ~/.config/xmobar && \
@@ -21,13 +24,17 @@ else
                  sudo rm -rf ~/lightdm && \
                  sudo rm -rf /etc/systemd/system/display-manager.service && \
                  sudo rm -rf /etc/systemd/system/sleep.target.wants && \
-                 rm -rf ~/.xinitrc
+                 rm -rf ~/.xinitrc && \
                  git clone https://github.com/TobbeBob123/.xmonad.git ~/.xmonad && \
                  git clone https://github.com/TobbeBob123/xmobar.git ~/.config/xmobar && \
                  git clone https://github.com/TobbeBob123/kitty.git ~/.config/kitty && \
                  git clone https://github.com/TobbeBob123/Bakgrunner.git ~/Bakgrunner && \
                  git clone https://github.com/TobbeBob123/Dracula_tema.git ~/.config/gtk-3.0 && \
                  git clone https://github.com/TobbeBob123/dunst.git ~/.config/dunst && \
+                 git clone https://github.com/TobbeBob123/Fish.git ~/.config/fish && \
+                 chsh -s /usr/bin/fish && \
+                 git clone https://github.com/TobbeBob123/starship.git ~/starship && \
+                 mv ~/starship/starship.toml ~/.config/starship.toml && \
                  git clone https://github.com/TobbeBob123/lightdm.git ~/lightdm && \
                  sudo mv ~/lightdm/display-manager.service /etc/systemd/system/display-manager.service && \
                  sudo mv ~/lightdm/sleep.target.wants/ /etc/systemd/system/sleep.target.wants/ && \
