67f44e06d55ee1cc077771a91706534a8353494d
Tobias <tobias.finsbol@gmail.com>
2022-05-09 14:35:49 +0200

    fjernPakker

--- fjernPakker.sh
+++ fjernPakker.sh
@@ -1,1 +1,1 @@
-sudo pacman -Rs eos-bash-shared eos-apps-info eos-rankmirrors eos-update-notifier reflector-simple welcom eos-log-tools
+sudo pacman -Rs eos-bash-shared eos-apps-info eos-rankmirrors eos-update-notifier reflector-simple welcome eos-log-tool
