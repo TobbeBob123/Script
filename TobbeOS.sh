f992ed1124d4900d6d9cf6112f5e9bbcb55bfc83
Tobias <tobias.finsbol@gmail.com>
2022-05-22 23:20:56 +0200

    emacs to TobbeOS

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -4,13 +4,13 @@
 echo "Du er i ferd med å overskrive xinit filen (.xinitrc). Vi anbefaler å ta bakup av innholdet av xinit filen."
 echo "Du burde kjøre fjernPakker.sh før du går videre"
 echo -n  "Vil du fortsette? [Y/n]: "
-read svar
+read -r svar
 if [[ $svar == "n" ]] || [[ $svar == "N" ]]
 then
     exit
 else
     # Setter doas istedenfor sudo
-    touch ~/doas.conf | echo permit :wheel > ~/doas.conf && sudo chown root:root ~/doas.conf && \
+    touch ~/doas.conf && echo permit :wheel > ~/doas.conf && sudo chown root:root ~/doas.conf && \
     sudo mv ~/doas.conf /etc/doas.conf && \
     # Oppgraderer og syncer databasene og installerer grunn pakkene for scriptet
     sudo pacman -Sy && sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib xmobar kakoune xorg xorg-xinit fish starship lib32-mesa && \
@@ -18,7 +18,7 @@ else
     sudo rm -rf yay-git ~/yay-git && \
     git clone https://aur.archlinux.org/yay-git.git ~/yay-git && cd ~/yay-git && makepkg -si && cd && rm -rf yay-git && \
     # Her lastes ned resten av programmene
-    yay -S htop kitty geary gnome-keyring rofi ttf-font-awesome-4 noto-fonts-emoji xdotool dracula-gtk-theme dracula-icons-git vifm network-manager-applet paru-bin adobe-source-code-pro-fonts pacman-contrib doas xautolock nodejs-lts-fermium lxsession dmenu exa lux-git trayer yad git jre-openjdk lightdm lightdm-gtk-greeter light-locker zip feh scrot dunst pavucontrol nm-connection-editor neovim libreoffice librewolf-bin signal-desktop teams-for-linux pulseaudio picom pcmanfm && \
+    yay -S htop kitty geary gnome-keyring rofi ttf-font-awesome-4 noto-fonts-emoji xdotool dracula-gtk-theme dracula-icons-git vifm network-manager-applet paru-bin adobe-source-code-pro-fonts pacman-contrib doas xautolock nodejs-lts-fermium lxsession dmenu exa lux-git trayer yad git jre-openjdk lightdm lightdm-gtk-greeter light-locker zip feh scrot dunst pavucontrol nm-connection-editor neovim libreoffice librewolf-bin signal-desktop teams-for-linux pulseaudio picom pcmanfm emacs ripgrep && \
                  # Her fjerner jeg mapper og filer som kan føre til error
                  rm -rf ~/.config/fish && \
                  rm -f ~/.fehbg && \
@@ -83,6 +83,12 @@ else
 		 # Her installerer jeg lightline i nvim
                  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
                 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && \
+                 #installerer doom emacs
+                 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d && \
+                 ~/.emacs.d/bin/doom install
+                 rm -rf ~/.doom.d
+                 git clone https://github.com/TobbeBob123/doom_emacs.git ~/doom.d
+                 ~/.emacs.d/bin/doom sync
                  # Her dras fish shell config til TobbeBob123
                  git clone https://github.com/TobbeBob123/Fish.git ~/.config/fish && \
                  # Her settes fish til hoved shell
@@ -108,9 +114,9 @@ else
                  # Her hentes rofi (program launcher) config fra git.
                  git clone https://github.com/TobbeBob123/Rofi.git ~/.config/rofi && \
                  # Her lages fil for å huske bakgrunnen og denne filen blir henta opp i xmonad config (~/.xmonad/xmonad.hs)
-                 touch ~/.fehbg | echo "feh --no-fehbg --bg-scale ~/Bakgrunner/Arch_Dracula.png" > ~/.fehbg && \
+                 touch ~/.fehbg && echo "feh --no-fehbg --bg-scale ~/Bakgrunner/Arch_Dracula.png" > ~/.fehbg && \
                  # Her lages en fil som lar brukeren bruke startx for å starte xmonad
-                 touch ~/.xinitrc | echo exec xmonad > ~/.xinitrc && \
+                 touch ~/.xinitrc && echo exec xmonad > ~/.xinitrc && \
                  # Her recompiles xmonad som tar i mot alt scriptet har gjort
                  xmonad --recompile && \
                  # Laster ned omf som brukes av fish
